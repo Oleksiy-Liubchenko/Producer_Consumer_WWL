6ba21fadbfc70a72f7fa6e6c113e7293f5d5eba1
Oleksiy Liubchenko <alexeylubchenko75@gmail.com>
2023-08-15 21:46:57 +0300

    Add crispy forms to project & add login.html

--- Producer_Consumer_WWL/settings.py
+++ Producer_Consumer_WWL/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'producer_consumer_app',
-    'bootstrap5'
+    'bootstrap5',
+    'crispy_forms',
+    'crispy_bootstrap5'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +128,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'producer_consumer_app.Employee'
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+LOGIN_REDIRECT_URL = "order_list"
+LOGOUT_REDIRECT_URL = "login"
--- Producer_Consumer_WWL/urls.py
+++ Producer_Consumer_WWL/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from producer_consumer_app.views import (
     OrderListView,
     delete_order
@@ -24,5 +24,6 @@ from producer_consumer_app.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('order_list/', OrderListView.as_view(), name="order_list"),
-    path('delete_order/<int:pk>/', delete_order, name="delete_order")
+    path('delete_order/<int:pk>/', delete_order, name="delete_order"),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
