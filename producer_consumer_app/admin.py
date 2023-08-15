9e896bb25cdc9e2c3657b9229f987acb69068bb1
Oleksiy Liubchenko <alexeylubchenko75@gmail.com>
2023-08-15 14:26:41 +0300

    Register admin models

--- producer_consumer_app/admin.py
+++ producer_consumer_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from producer_consumer_app.models import Order, Employee
+
+admin.site.register(Order)
+admin.site.register(Employee)
