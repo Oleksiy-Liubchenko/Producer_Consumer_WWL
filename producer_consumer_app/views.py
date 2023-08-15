f59528b59a9f9d91f628af3387ebfe5893189251
Oleksiy Liubchenko <alexeylubchenko75@gmail.com>
2023-08-15 22:16:15 +0300

    Update OrderListView with user queryset

--- producer_consumer_app/views.py
+++ producer_consumer_app/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView
 from producer_consumer_app.models import Order
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class OrderListView(ListView):
+class OrderListView(ListView, LoginRequiredMixin):
     model = Order
     template_name = "producer_consumer_app/order_list.html"
     context_object_name = "orders"
+
+    def get_queryset(self):
+        return Order.objects.filter(employee__id=self.request.user.id)
 
 
 def delete_order(request, pk):
