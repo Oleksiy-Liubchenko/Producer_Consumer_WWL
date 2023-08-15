883429043a98a28a9dfc2041b7dc20b4cd7f00c5
Oleksiy Liubchenko <alexeylubchenko75@gmail.com>
2023-08-15 16:12:21 +0300

    Update views.py with OrderList&Delete_order views

--- producer_consumer_app/views.py
+++ producer_consumer_app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView
+from producer_consumer_app.models import Order
 
-# Create your views here.
+
+class OrderListView(ListView):
+    model = Order
+    template_name = "producer_consumer_app/order_list.html"
+    context_object_name = "orders"
+
+
+def delete_order(request, pk):
+    order = get_object_or_404(Order, pk=pk)
+    if request.method == "POST":
+        order.delete()
+        return redirect("order_list")
+    return render(request, "producer_consumer_app/order_list.html")
