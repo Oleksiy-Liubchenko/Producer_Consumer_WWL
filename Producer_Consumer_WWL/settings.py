cc0d45964b19470541bc029b77e0f65031ffeb94
Oleksiy Liubchenko <alexeylubchenko75@gmail.com>
2023-08-16 21:46:54 +0300

    Connect Posgres + Update DB settings & Update .env

--- Producer_Consumer_WWL/settings.py
+++ Producer_Consumer_WWL/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xute#xcy&^%z4lppe()%$m$!jy162^t(_vfyt)a+0#3+eu21_y'  #TODO: regen and import to .env
+SECRET_KEY = os.getenv("PROJECT_SECRET_KEY")  #TODO: regen and import to .env
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -84,9 +84,13 @@ WSGI_APPLICATION = 'Producer_Consumer_WWL.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
