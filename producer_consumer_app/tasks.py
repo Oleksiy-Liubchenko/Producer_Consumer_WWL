e917e1e8c605d3b126bbe19577235daaaebb211c
Oleksiy Liubchenko <alexeylubchenko75@gmail.com>
2023-08-16 21:50:03 +0300

    Delete comments in tasks.py & Update requirements.txt

--- producer_consumer_app/tasks.py
+++ producer_consumer_app/tasks.py
@@ -3,22 +3,6 @@ from producer_consumer_app.models import Order, Employee
 
 from celery import shared_task
 
-#docker run -d -p 6379:6379 redis
-# celery -A Producer_Consumer_WWL worker -l INFO
-
-# if without redis docker command error:  ERROR/MainProcess] consumer: Cannot connect to redis://localhost:6379//: Error 10061 connecting to localhost:6379. Подключение не установлено, т.к. конечный компьютер отверг запрос на подключение..
-# Trying again in 2.00 seconds... (1/100)
-
-# from producer_consumer_app.tasks import count_orders
-# pip install eventlet
-# celery -A Producer_Consumer_WWL worker -l info -P eventlet
-# celery -A Producer_Consumer_WWL beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler
-
-# @shared_task
-# def count_orders():
-#     return Order.objects.count()
-
-# TODO: make upd 5 min, not 5 sec
 
 @shared_task
 def order_creation() -> None:
